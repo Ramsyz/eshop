18590643f76dcdda49e2229203cf357570cf6acb
Ramsyz <seetaram.vannemreddi@gmail.com>
2023-07-03 13:38:37 -0400

    CartItem redux persist

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import eshopReducer from "./eshopSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, eshopReducer);
+
 export const store = configureStore({
-  reducer: {
-    eshop: eshopReducer,
-  },
+  reducer: { eshop: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
